7c393782fd378bbb7eec04a0701b011ebb9b85c3
Luis Ricardo <luiscastrodev@gmail.com>
2023-08-04 07:05:50 -0300

    config room database
    config room database

--- app/src/main/java/br/com/example/todoappcompose/MainActivity.kt
+++ app/src/main/java/br/com/example/todoappcompose/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import br.com.example.todoappcompose.ui.theme.ToDoAppComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
